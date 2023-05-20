4ed9432f62846b40811d787132d4bc5ce002277f
Agrim Chaudhary <102463391+agrim106@users.noreply.github.com>
2023-05-20 14:18:24 +0530

    Add files via upload

---
+++ asgnop.java
@@ -0,0 +1,7 @@
+public class asgnop {
+public static void main (String[] args) {
+	int x = 5454;
+	x *= 45;
+	System.out.println(x);
+ }
+}
---
+++ condtnstetments.java
@@ -0,0 +1,11 @@
+public class condtnstmnts{
+public static void main(String[] args){
+	int x = 56;
+	int y = 20;
+	int z = x*y;
+	
+	if((z) <= 6000){
+	System.out.println("balance is low");
+  }
+ }
+}
