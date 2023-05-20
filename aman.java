ff5eaf41da221b2344f0929a6e8a4be463ecc69b
Agrim Chaudhary <102463391+agrim106@users.noreply.github.com>
2023-05-20 13:45:03 +0530

    Add files via upload
    Aman is my  first java file

---
+++ aman.java
@@ -0,0 +1,8 @@
+public class aman {
+public static void main (String[] args) {
+	int myNum = 65;
+	String name = "aman is son of agrim";
+	System.out.println(myNum);
+	System.out.println(name);
+ }
+}
