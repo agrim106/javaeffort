4ffb4a25527a8b1831d96ff1e4ba3f3cb5ce69c0
Agrim Chaudhary <102463391+agrim106@users.noreply.github.com>
2023-05-22 09:48:10 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,7 @@
+public class operators {
+public static void main(String[] args) {
+	int x = 454;
+	int y = 234;
+	System.out.println(x*y);
+ }
+}
---
+++ practice.java
@@ -0,0 +1,6 @@
+public class practice {
+public static void main(String[] args){
+	long myNum = 15004798465L;
+	System.out.println(myNum);
+ }
+}
