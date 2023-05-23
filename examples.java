6aed0e5b836b734488e05c1e7a62caca01385f96
Agrim Chaudhary <102463391+agrim106@users.noreply.github.com>
2023-05-23 15:29:34 +0530

    Add files via upload
    Final files of initial practices

---
+++ examples.java
@@ -0,0 +1,6 @@
+public class examples{
+public static void main(String[] args){
+	byte myNum = 120;
+	System.out.println(myNum);
+ }
+}
---
+++ fifteenmay.java
@@ -0,0 +1,6 @@
+public class fifteenmay {
+public static void main(String[] args){
+	float myNum = 45.325f;
+	System.out.println(myNum);
+ }
+}
